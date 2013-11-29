a8359c005e0494a344e999b674846364dc751627
Roberto Caldas <roberto.c@markermetro.com>
2013-11-29 16:53:53 +1300

    Changed projects folders name.

---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Threading/Thread.cs
@@ -0,0 +1,11 @@
+﻿namespace MarkerMetro.Unity.WinLegacy.Threading
+{
+    public class Thread
+    {
+        public static void Sleep(int ms)
+        {
+            new System.Threading.ManualResetEvent(false).WaitOne(ms);
+        }
+    }
+}
+
