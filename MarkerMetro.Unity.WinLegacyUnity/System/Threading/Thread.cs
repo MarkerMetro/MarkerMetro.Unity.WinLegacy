c437d88001db43907385249d9df70ce71238e833
Greg Quinn <greg.q@marker.co.nz>
2013-12-04 16:05:25 +1300

    Added Thread class implementation

--- MarkerMetro.Unity.WinLegacyUnity/System/Threading/Thread.cs
+++ MarkerMetro.Unity.WinLegacyUnity/System/Threading/Thread.cs
@@ -1,11 +1,54 @@
-﻿namespace MarkerMetro.Unity.WinLegacy.Threading
+﻿using System;
+using System.Threading;
+
+namespace MarkerMetro.Unity.WinLegacy.Threading
 {
+    public delegate void ParameterizedThreadStart(object target);
+    public delegate void ThreadStart();
+
     public class Thread
     {
+        public bool IsBackground { get; set; }
+
+        public Thread(ThreadStart start)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Thread(ParameterizedThreadStart start)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Abort()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Join(int ms)
+        {
+            return false;
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(Object thread)
+        {
+            throw new NotImplementedException();
+        }
+
         public static void Sleep(int ms)
         {
-            new System.Threading.ManualResetEvent(false).WaitOne(ms);
+            new ManualResetEvent(false).WaitOne(ms);
         }
+    }
+
+    public class ThreadAbortException : Exception
+    {
+        
     }
 }
 
