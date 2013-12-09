5d68a2990d312b0a640ce00f14053b868631de32
Greg Quinn <greg.q@marker.co.nz>
2013-12-10 11:30:03 +1300

    Removed Stream and MemoryStream and implemented FileStream based on Stream members

--- MarkerMetro.Unity.WinLegacyUnity/System/IO/FileStream.cs
+++ MarkerMetro.Unity.WinLegacyUnity/System/IO/FileStream.cs
@@ -1,28 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using MarkerMetro.Unity.WinLegacy.IO;
 
-//namespace MarkerMetro.Unity.WinLegacy.IO
-//{
-//    public class FileStream : Stream
-//    {
-//        public long Length { get; set; }
+namespace MarkerMetro.Unity.WinLegacy.IO
+{
+    public class FileStream : Stream
+    {
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
 
-//        public void Write(byte[] array, int offset, int count)
-//        {
-            
-//        }
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
 
-//        public void Read(byte[] array, int offset, int count)
-//        {
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
 
-//        }
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
 
-//        public void Close()
-//        {
-            
-//        }
-//    }
-//}
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanRead
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool CanSeek
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool CanWrite
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override long Length
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override long Position { get; set; }
+    }
+}
