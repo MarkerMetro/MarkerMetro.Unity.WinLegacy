1ed563495b0f8fa94fb95f39686190c0e3084d94
Roberto Caldas <roberto.c@markermetro.com>
2013-12-10 16:49:44 +1300

    Added System.Diagnostics.StackFrame stub class.

---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Diagnostics/StackFrame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MarkerMetro.Unity.WinLegacy.Diagnostics
+{
+    public class StackFrame
+    {
+        public string GetFileName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFileLineNumber()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
