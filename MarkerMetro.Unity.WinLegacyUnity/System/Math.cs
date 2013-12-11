14bdf02ada6f4a372c97875e3dd1c2e4e06bb8c6
Roberto Caldas <roberto.c@markermetro.com>
2013-12-11 19:27:20 +1300

    Added an implementation for Math.DivRem(int, int, out int).

---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Math.cs
@@ -0,0 +1,12 @@
+﻿namespace MarkerMetro.Unity.WinLegacy
+{
+    public static class Math
+    {
+        public static int DivRem(int a, int b, out int result)
+        {
+            int quotient = a / b;
+            result = a - b * quotient;
+            return quotient;
+        }
+    }
+}
