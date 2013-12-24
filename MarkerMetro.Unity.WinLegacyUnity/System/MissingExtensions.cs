3e12ea2f6daaeff29e2b2f6fcc7cffd6665a6038
Roberto Caldas <roberto.c@markermetro.com>
2013-12-24 19:20:48 +1300

    Moved List extension methods to a CollectionsExtensions.cs file, since WP8 project isn't supposed to have MissingExtensions in it.

--- MarkerMetro.Unity.WinLegacyUnity/System/MissingExtensions.cs
+++ MarkerMetro.Unity.WinLegacyUnity/System/MissingExtensions.cs
@@ -8,11 +8,6 @@ using System.Runtime.InteropServices;
 
 namespace MarkerMetro.Unity.WinLegacy
 {
-    /** 
-     * Converter is not supported in Win8 or WP8.1, but supported in WP8.
-     */
-    public delegate TOutput Converter<in TInput, out TOutput>(TInput input);
-
     /**
      * Helpers for missing functions of classes, turned into extensions instead...
     */
@@ -45,21 +40,6 @@ namespace MarkerMetro.Unity.WinLegacy
             foreach (T obj in list)
                 action(obj);
         }
-
-        /**
-         * List<TOutput>.ConvertAll<TOutput>(Converter) is neither implement in WSA nor in WP8.
-         */
-        public static List<TOutput> ConvertAll<T, TOutput>(this List<T> list, Converter<T, TOutput> converter)
-        {
-            if (converter == null)
-                throw new ArgumentNullException("converter is null.");
-            
-            List<TOutput> result = new List<TOutput>(list.Count);
-            foreach (T t in list)
-                result.Add(converter(t));
-            return result;
-        }
-
     }
 }
 
