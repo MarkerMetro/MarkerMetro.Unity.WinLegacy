c04db6a37f615a4201bc38b7c6af05c3ed0cbb9c
Ivan Živković <megame@outlook.com>
2014-01-22 12:17:06 +1300

    Added WebRequest and NVC, WHC

---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Net/WebHeaderCollection.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarkerMetro.Unity.WinLegacy.Net
+{
+    public class WebHeaderCollection : MarkerMetro.Unity.WinLegacy.Collections.Specialized.NameValueCollection 
+    {
+        readonly System.Net.WebHeaderCollection _actual;
+        internal WebHeaderCollection(System.Net.WebHeaderCollection actual)
+        {
+            if (actual == null)
+                throw new ArgumentNullException("actual", "actual is null.");
+
+            _actual = actual;
+        }
+
+        public override void Add(string name, string value)
+        {
+            _actual.Add(name, value);
+        }
+
+        public override string[] AllKeys
+        {
+            get { return _actual.AllKeys; }
+        }
+
+        public override void Clear()
+        {
+            _actual.Clear();
+        }
+
+        public override string Get(int index)
+        {
+            return _actual.Get(index);
+        }
+
+        public override string Get(string name)
+        {
+            return _actual.Get(name);
+        }
+
+        public override string GetKey(int index)
+        {
+            return _actual.GetKey(index);
+        }
+
+        public override string[] GetValues(int index)
+        {
+            return _actual.GetValues(index);
+        }
+
+        public override string[] GetValues(string name)
+        {
+            return _actual.GetValues(name);
+        }
+
+        public override void Remove(string name)
+        {
+            _actual.Remove(name);
+        }
+
+        public override void Set(string name, string value)
+        {
+            _actual.Set(name, value);
+        }
+
+        public override System.Collections.IEnumerator GetEnumerator()
+        {
+            return _actual.GetEnumerator();
+        }
+    }
+}
---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Net/WebRequest.cs
@@ -0,0 +1,75 @@
+﻿using MarkerMetro.Unity.WinLegacy.Collections.Specialized;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarkerMetro.Unity.WinLegacy.Net
+{
+    public class WebRequest : System.Net.WebRequest
+    {
+        readonly System.Net.WebRequest _actual;
+
+        private WebRequest(System.Net.WebRequest actaul)
+        {
+            if (actaul == null)
+                throw new ArgumentNullException("actaul", "actaul is null.");
+
+            _actual = actaul;
+        }
+
+        public new NameValueCollection Headers
+        {
+            get
+            {   // TODO: This will not work as setting values should set values in collection below:
+                return new WebHeaderCollection(_actual.Headers);
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        // TODO: Add Headers somehow?
+        new public static WebRequest Create(string requestUriString)
+        {
+            var actual = System.Net.WebRequest.Create(requestUriString);
+
+            return new WebRequest(actual);
+        }
+
+        new public static WebRequest Create(Uri requestUri)
+        {
+            var actual = System.Net.WebRequest.Create(requestUri);
+
+            return new WebRequest(actual);
+        }
+
+#if NETFX_CORE || SILVERLIGHT
+        int _timeout;   // TODO: Set default value
+#endif
+        public virtual int Timeout
+        {
+            get
+            {
+#if NETFX_CORE || SILVERLIGHT
+                return _timeout;
+#else
+                return base.Timeout;
+#endif
+            }
+            set
+            {
+                if (value < -1)
+                    throw new ArgumentOutOfRangeException("value");
+
+#if NETFX_CORE || SILVERLIGHT
+                // TODO: check range
+                _timeout = value;
+#else
+                base.Timeout = value;
+#endif
+            }
+        }
+    }
+}
