8504c9b476e58ed4d0fd3591cb46fbbe3bfec241
Greg Quinn <greg.q@marker.co.nz>
2014-01-21 16:13:02 +1300

    Added System.NET skeleton classes

---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Net/WebException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MarkerMetro.Unity.WinLegacy.Net
+{
+    public enum WebExceptionStatus { ProtocolError }
+
+    public class WebException
+    {
+        public WebException()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Net/WebRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MarkerMetro.Unity.WinLegacy.Net
+{
+#if UNITY_METRO
+    [SerializableAttribute]
+    public abstract class WebRequest : MarshalByRefObject, ISerializable
+    {
+        public virtual int Timeout { get; set; }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+#endif
+}
