002828ee61c96634aa47daf800b86dd825422ce9
Ivan Živković <megame@outlook.com>
2014-03-04 12:04:59 +1300

    Added SortedList

---
+++ MarkerMetro.Unity.WinLegacyUnity.Tests/SortedListTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarkerMetro.Unity.WinLegacyUnity.Tests
+{
+    [TestClass]
+    public class SortedListTests
+    {
+        [TestMethod]
+        public void IntegerSortedList_ShouldBehaveSame()
+        {
+            var sysList = new System.Collections.SortedList();
+            var mmList = new MarkerMetro.Unity.WinLegacy.Collections.SortedList();
+
+            sysList.Add(1, "one");
+            sysList.Add(0, "zero");
+            sysList.Add(2, "two");
+
+            mmList.Add(1, "one");
+            mmList.Add(0, "zero");
+            mmList.Add(2, "two");
+
+            CollectionAssert.AreEquivalent(sysList.Values, mmList.Values);
+        }
+
+        [TestMethod]
+        public void IntegerSortedList_SecondEleemntByIndex_ShouldBeTheSame()
+        {
+            var sysList = new System.Collections.SortedList();
+            var mmList = new MarkerMetro.Unity.WinLegacy.Collections.SortedList();
+
+            sysList.Add(1, "one");
+            sysList.Add(0, "zero");
+            sysList.Add(2, "two");
+
+            mmList.Add(1, "one");
+            mmList.Add(0, "zero");
+            mmList.Add(2, "two");
+
+            Assert.AreEqual(sysList.GetByIndex(1), mmList.GetByIndex(1));
+        }
+    }
+}
---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Collections/SortedList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarkerMetro.Unity.WinLegacy.Collections
+{
+    public class SortedList : SortedDictionary<object, object>
+    {
+        public virtual object GetByIndex(int index)
+        {
+            if(index<0 && index>base.Count)
+                throw new ArgumentOutOfRangeException();
+
+            return base.Values.Skip(index).First();
+        }
+    }
+}
