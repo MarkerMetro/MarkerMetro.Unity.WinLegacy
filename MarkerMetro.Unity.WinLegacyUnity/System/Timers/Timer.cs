28ddd489ac0afd6568ddffd6702e5a8c2dfcc392
Roberto Caldas <roberto.c@markermetro.com>
2014-03-11 14:30:12 +1300

    Added `System.Timers` classes placeholders.

---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Timers/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MarkerMetro.Unity.WinLegacy.IO.Timers
+{
+    public delegate void ElapsedEventHandler(Object sender,	ElapsedEventArgs e);
+    public class Timer
+    {
+        public event ElapsedEventHandler Elapsed;
+        public Timer(double interval)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class ElapsedEventArgs : EventArgs
+    {
+        public DateTime SignalTime { get; }
+    }
+}
