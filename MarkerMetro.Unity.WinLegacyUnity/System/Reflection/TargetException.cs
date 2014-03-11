a069e28c8626ea75733928da69ca599cd640b473
Roberto Caldas <roberto.c@markermetro.com>
2014-03-12 11:24:54 +1300

    Added `System.Reflection.TargetException`.

---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Reflection/TargetException.cs
@@ -0,0 +1,11 @@
+﻿#if NETFX_CORE
+
+namespace MarkerMetro.Unity.WinLegacy.Reflection
+{
+    class TargetException : System.Exception
+    {
+        public TargetException() : base() { }
+        public TargetException(string message) : base(message) { }
+    }
+}
+#endif
