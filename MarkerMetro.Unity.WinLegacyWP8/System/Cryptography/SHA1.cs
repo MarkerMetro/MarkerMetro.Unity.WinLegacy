f17acece58bbdfa6fd49f362f0d8dec58e1cb025
Michael Quandt <michael@mquandt.com>
2014-09-09 16:53:55 -0700

    Added SHA1 support to WP8

---
+++ MarkerMetro.Unity.WinLegacyWP8/System/Cryptography/SHA1.cs
@@ -0,0 +1,48 @@
+﻿#if NETFX_CORE
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+#endif
+
+using System;
+
+namespace MarkerMetro.Unity.WinLegacy.Cryptography
+{
+    public class SHA1 : IDisposable
+    {
+
+        public static SHA1 Create()
+        {
+            return new SHA1();
+        }
+
+        public void Dispose() { }
+
+#if NETFX_CORE        
+        private HashAlgorithmProvider hap;
+
+        private SHA1()
+        {
+            hap = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Sha1);
+        }
+
+        public byte[] ComputeHash(byte[] buffer)
+        {
+            IBuffer buffInput = CryptographicBuffer.CreateFromByteArray(buffer);
+            IBuffer buffHash = hap.HashData(buffInput);
+
+            byte[] hash;
+            CryptographicBuffer.CopyToByteArray(buffHash, out hash);
+            return hash;
+        }
+#elif WINDOWS_PHONE
+        public byte[] ComputeHash(byte[] buffer)
+        {
+            using (var sha = new System.Security.Cryptography.SHA1Managed())
+                return sha.ComputeHash(buffer);
+        }
+#else
+        public byte[] ComputeHash(byte[] buffer) { throw new System.NotImplementedException(); }
+#endif
+    }
+}
