106378f4cbdd85f8e117a24e0b10bd7cb51343dc
Ruslan Idrisov <ruslan.i@markermetro.com>
2015-04-07 16:00:49 +1200

    - Added HmacSHA256 algorithm (and corresponding unit test); - removed windows phone ifdef from on cryptographytest.cs tests

--- MarkerMetro.Unity.WinLegacyUnity.Tests/System/Security/Cryptography/CryptographyTests.cs
+++ MarkerMetro.Unity.WinLegacyUnity.Tests/System/Security/Cryptography/CryptographyTests.cs
@@ -14,27 +14,12 @@ namespace MarkerMetro.Unity.WinLegacy.Security.Cryptography.Tests
     [TestClass]
     public class CryptographyTests
     {
-#if WINDOWS_PHONE
-        /// <summary>
-        /// Test MD5.GetMd5String should equal expected result.
-        /// </summary>
-        [TestMethod]
-        public void WP8MD5GetMd5String_MarkerMetro_Succeed()
-        {
-            Assert.IsTrue(MD5.GetMd5String("MarkerMetro") == "9c33c811d0c45c41cb66b393ec9f7ed7");
-        }
-#endif
-
-#if NETFX_CORE || WINDOWS_PHONE
+#if NETFX_CORE
         /// <summary>
         /// Test SHA1.ComputeHash should equal expected result.
         /// </summary>
         [TestMethod]
-#if NETFX_CORE
         public void MetroSHA1ComputeHash_MarkerMetro_Succeed()
-#else
-        public void WP8SHA1ComputeHash_MarkerMetro_Succeed()
-#endif
         {
             SHA1 sha1 = SHA1.Create();
 
@@ -49,16 +34,32 @@ namespace MarkerMetro.Unity.WinLegacy.Security.Cryptography.Tests
 
             Assert.IsTrue(sb.ToString() == "6d0abfbda56702bd259dfb262383329af17f8eb4");
         }
+        
+
+        [TestMethod]
+        public void MetroHmacSHA256ComputeHash_MarkerMetro_Succeed()
+        {
+            HmacSHA256 hmac = HmacSHA256.Create();
+
+            byte[] hash = hmac.ComputeHash("MarkerMetro", "secret");
+
+            var sb = new StringBuilder(hash.Length * 2);
+            foreach (byte b in hash)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+
+            string hashString = sb.ToString();
+
+            Assert.IsTrue(sb.ToString() == "fa2383f663bbe784c7e5d9076755e37cd3dcf09e4313d40eb3ae86ba2bcb8dde");
+        }
 
         /// <summary>
         /// Test EncryptionProvider encrypt and decrypt.
         /// </summary>
         [TestMethod]
-#if NETFX_CORE
+
         public void MetroEncryptionProvider_EncryptDecrypt_ShouldEqualOriginal()
-#else
-        public void WP8EncryptionProvider_EncryptDecrypt_ShouldEqualOriginal()
-#endif
         {
             string originalString = "MarkerMetro";
             string key = "encryptionKey";
---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Security/Cryptography/HmacSHA256.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#if NETFX_CORE
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+using System.Runtime.InteropServices.WindowsRuntime;
+#endif
+
+namespace MarkerMetro.Unity.WinLegacy.Security.Cryptography
+{
+    public class HmacSHA256 : IDisposable
+    {
+        public static HmacSHA256 Create()
+        {
+            return new HmacSHA256();
+        }
+
+        public void Dispose() { }
+
+#if NETFX_CORE        
+        private MacAlgorithmProvider hap;
+
+        private HmacSHA256()
+        {
+            //creates algorithm provider
+            hap = MacAlgorithmProvider.OpenAlgorithm(MacAlgorithmNames.HmacSha256);
+        }
+
+        /// <summary>
+        /// takes in a UTF8 byte array, returns hashed byte array using HmacSHA256
+        /// </summary>
+        /// <param name="buffer"> UTF8 byte array</param>
+        /// <param name="cryptSecret"> a secret key to be used in hash function</param>
+        /// <returns>hashed byte array</returns>
+        public byte[] ComputeHash(byte[] buffer, string cryptSecret)
+        {
+            //creates algorithm with secret provided
+            var hmacAlg = hap.CreateHash(Encoding.UTF8.GetBytes(cryptSecret).AsBuffer());
+            //adds data
+            hmacAlg.Append(CryptographicBuffer.CreateFromByteArray(buffer));
+            byte[] hash;
+            //converts to byte array
+            CryptographicBuffer.CopyToByteArray(hmacAlg.GetValueAndReset(), out hash);
+            return hash;
+        }
+
+        /// <summary>
+        /// takes in a string, encodes into UTF8 and returns hash
+        /// </summary>
+        /// <param name="value"> string data to be converted into UTF8 </param>
+        /// <param name="cryptSecret"> a secret key to be used in hash function</param>
+        /// <returns>hashed byte array</returns>
+        public byte[] ComputeHash(string value, string cryptSecret)
+        {
+            return ComputeHash(Encoding.UTF8.GetBytes(value), cryptSecret);
+        }
+#else
+        public byte[] ComputeHash(byte[] buffer) { throw new System.NotImplementedException(); }
+#endif
+    }
+}
