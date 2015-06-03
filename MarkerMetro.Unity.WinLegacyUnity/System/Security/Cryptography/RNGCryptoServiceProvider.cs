2354a49d9dd160ee407fb4e5e538719e89331424
Freddy Weng <hungweng@gmail.com>
2015-06-03 12:41:41 +1200

    Add RNGCryptoServiceProvider

---
+++ MarkerMetro.Unity.WinLegacyUnity/System/Security/Cryptography/RNGCryptoServiceProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if NETFX_CORE
+using Windows.Security.Cryptography;
+using System.Runtime.InteropServices.WindowsRuntime;
+#endif
+
+namespace MarkerMetro.Unity.WinLegacy.Security.Cryptography
+{
+    class RNGCryptoServiceProvider
+    {
+        public void GetBytes (byte[] buffer)
+        {
+#if NETFX_CORE
+            CryptographicBuffer.GenerateRandom((uint)buffer.Length).ToArray().CopyTo(buffer, 0);
+#else
+            throw new PlatformNotSupportedException();
+#endif
+        }
+    }
+}
