62b2ee92eb230dd2d3fe37b3b4e095bd05e7623a
Freddy Weng <freddy.w@markermetro.com>
2015-12-14 14:28:16 +1300

    Add reflection extension method to get MemberType from MemberInfo

--- MarkerMetro.Unity.WinLegacyUnity/System/Reflection/ReflectionExtensions.cs
+++ MarkerMetro.Unity.WinLegacyUnity/System/Reflection/ReflectionExtensions.cs
@@ -42,6 +42,54 @@ namespace MarkerMetro.Unity.WinLegacy.Reflection
         Object
     }
 
+    // Summary:
+    //     Marks each type of member that is defined as a derived class of MemberInfo.
+    [Flags]
+    public enum MemberTypes
+    {
+        // Summary:
+        //     Specifies that the member is a constructor, representing a System.Reflection.ConstructorInfo
+        //     member. Hexadecimal value of 0x01.
+        Constructor = 1,
+        //
+        // Summary:
+        //     Specifies that the member is an event, representing an System.Reflection.EventInfo
+        //     member. Hexadecimal value of 0x02.
+        Event = 2,
+        //
+        // Summary:
+        //     Specifies that the member is a field, representing a System.Reflection.FieldInfo
+        //     member. Hexadecimal value of 0x04.
+        Field = 4,
+        //
+        // Summary:
+        //     Specifies that the member is a method, representing a System.Reflection.MethodInfo
+        //     member. Hexadecimal value of 0x08.
+        Method = 8,
+        //
+        // Summary:
+        //     Specifies that the member is a property, representing a System.Reflection.PropertyInfo
+        //     member. Hexadecimal value of 0x10.
+        Property = 16,
+        //
+        // Summary:
+        //     Specifies that the member is a type, representing a System.Reflection.MemberTypes.TypeInfo
+        //     member. Hexadecimal value of 0x20.
+        TypeInfo = 32,
+        //
+        // Summary:
+        //     Specifies that the member is a custom member type. Hexadecimal value of 0x40.
+        Custom = 64,
+        //
+        // Summary:
+        //     Specifies that the member is a nested type, extending System.Reflection.MemberInfo.
+        NestedType = 128,
+        //
+        // Summary:
+        //     Specifies all member types.
+        All = 191,
+    }
+
     public static class ReflectionExtensions
     {
 
@@ -592,6 +640,28 @@ namespace MarkerMetro.Unity.WinLegacy.Reflection
             return result;
         }
 
-    }
+        public static MemberTypes GetMemberType(this MemberInfo member)
+        {
+#if NETFX_CORE
+            if (member is FieldInfo)
+                return MemberTypes.Field;
+            if (member is ConstructorInfo)
+                return MemberTypes.Constructor;
+            if (member is PropertyInfo)
+                return MemberTypes.Property;
+            if (member is EventInfo)
+                return MemberTypes.Event;
+            if (member is MethodInfo)
+                return MemberTypes.Method;
 
+            var typeInfo = member as TypeInfo;
+            if (!typeInfo.IsPublic && !typeInfo.IsNotPublic)
+                return MemberTypes.NestedType;
+
+            return MemberTypes.TypeInfo;
+#else
+            throw new NotImplementedException();
+#endif
+        }
+    }
 }
